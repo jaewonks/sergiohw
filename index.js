af3408d3d98f47903fc44c8e9a61fbfc929fa5c8
Jaewon Kim <jaewon@jaewons-mbp.home>
2021-01-08 10:30:14 +0000

    14

--- index.js
+++ index.js
@@ -9,11 +9,9 @@ function myFunction() {
 }
 
 function init() {
-	var vidDefer = document.getElementsByTagName('iframe');
-	for (var i=0; i<vidDefer.length; i++) {
-	if(vidDefer[i].getAttribute('data-src')) {
-	vidDefer[i].setAttribute('src',vidDefer[i].getAttribute('data-src'));
+  var vidDefer = document.getElementsByTagName('iframe');
+    if(vidDefer.getAttribute('data-src')) {
+      vidDefer.setAttribute('src',vidDefer[i].getAttribute('data-src'));
+		} 
 	} 
-	} 
-}
 window.onload = init;
