6400c5a871c54fa0c69109750e5a80e9959bb5b5
Jaewon Kim <jaewon@jaewons-mbp.home>
2021-01-08 12:59:26 +0000

    rere

--- index.js
+++ index.js
@@ -7,11 +7,3 @@ function myFunction() {
 	  x.className = "topnav";
 	}
 }
-
-// document.addEventListener('touchstart', onTouchStart, {passive: true});
-
-document.addEventListener("touchstart", function(e) {
-	console.log(e.defaultPrevented);  // will be false
-	e.preventDefault();   // does nothing since the listener is passive
-	console.log(e.defaultPrevented);  // still false
-}, Modernizr.passiveeventlisteners ? {passive: true} : false);
